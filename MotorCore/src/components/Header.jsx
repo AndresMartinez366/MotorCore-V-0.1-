fcb8b56a8be41fd8b8ea0dfb6ba5d958b35b50df
Andres <jonathanandresmartinez366@gmail.com>
2026-04-16 22:02:50 +0000

    V.0.3.0

--- MotorCore/src/components/Header.jsx
+++ MotorCore/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo2.png";
 
 function Header() {
   const [search, setSearch] = useState("");
@@ -11,24 +11,22 @@ function Header() {
 
   return (
     <header className="header">
-    
-    
       <div className="brand">
         <img src={logo} alt="Logo MotorCore" className="logo" />
-        <h1>MotorCore</h1>
+        <h1 className="brand-title">
+          Motor<span className="brand-core">Core</span>
+        </h1>
       </div>
 
-    
       <div className="search-box">
         <input
           type="text"
-          placeholder="Buscar motos, repuestos o reparaciones…"
+          placeholder="Buscar motos, repuestos o reparaciones..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
       </div>
 
-    
       <div className="user-info">
         <span className="user-name">{user.name}</span>
         <span className="user-role">{user.role}</span>
@@ -37,4 +35,4 @@ function Header() {
   );
 }
 
-export default Header;
+export default Header;  
